6707a32b85cdc7250e45023cd69299e426dc32d0
nirmaman <nirmaman631@gmail.com>
2022-01-23 10:31:32 +0200

    Add Functionality to login page, connect project to firebase.

--- js/login.js
+++ js/login.js
@@ -1,31 +1,42 @@
-// Import the functions you need from the SDKs you need
-// import { getAnalytics } from 'firebase/analytics'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {
+firebase.initializeApp({
   apiKey: 'AIzaSyCxzOG5wE6-A9PTPlQUSJEGHoz1Acbetm8',
   authDomain: 'socialnetworksbots.firebaseapp.com',
   projectId: 'socialnetworksbots',
-  storageBucket: 'socialnetworksbots.appspot.com',
-  messagingSenderId: '180389196591',
-  appId: '1:180389196591:web:a00c8fc35f5c496e55e7da',
-  measurementId: 'G-XN1SBKC3B4',
+  // storageBucket: 'socialnetworksbots.appspot.com',
+  // messagingSenderId: '180389196591',
+  // appId: '1:180389196591:web:a00c8fc35f5c496e55e7da',
+  // measurementId: 'G-XN1SBKC3B4',
+})
+var db = firebase.firestore()
+
+async function submitForm() {
+  let userName = document.getElementById('username').value
+  let password = document.getElementById('password').value
+
+  const usersRef = db.collection('usersBot')
+  const snapshot = await usersRef.get()
+  snapshot.forEach((doc) => {
+    //console.log(doc.data().userName);
+    if (userName === doc.data().userName && password === doc.data().password) {
+      window.location.href = '/mainPage'
+      return
+    }
+  })
+  document.getElementById('wrong_option').innerHTML += '*Emploee not found'
+  console.log('not found')
 }
 
-//Initialize Firebase
-// firebase.initializeApp(firebaseConfig)
-// firebase.analytics()
-
-
-login = function () {
-  alert('nir')
-  // firebase
-  //   .auth()
-  //   .signInWithEmailAndPassword(userEmail, userPass)
-  //   .than(async function (firebaseUser) {
-  //     console.log(firebaseUser.userBot)
-  //   })
+function add_emploee() {
+  db.collection('users')
+    .add({
+      first: 'Ada',
+      last: 'Lovelace',
+      born: 1815,
+    })
+    .then((docRef) => {
+      console.log('Document written with ID: ', docRef.id)
+    })
+    .catch((error) => {
+      console.error('Error adding document: ', error)
+    })
 }
