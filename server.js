153b8dd12716ffdb3658f198b73f74cb5bc5b6ec
nirmaman <nirmaman631@gmail.com>
2022-03-14 13:47:43 +0200

    cancel port 3002

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 ;(path = require('path')), (fs = require('fs')), (cors = require('cors')), (routers = require('./routes/routes.js'))
-const port = process.env.PORT || 3002
+const port = process.env.PORT
 const host = '0.0.0.0'
 const app = express()
 
@@ -8,7 +8,7 @@ app.use('/', express.static(path.join(__dirname, 'html')))
 app.use('/MessageOnEvent', express.static(path.join(__dirname, 'html/messageOnEvent.html')))
 app.use('/mainPage', express.static(path.join(__dirname, 'html/mainPage.html')))
 app.use('/addConnections', express.static(path.join(__dirname, 'html/addConnections.html')))
-app.use('/login', express.static(path.join(__dirname, 'html/login.html')))
+app.use('/index', express.static(path.join(__dirname, 'html/index.html')))
 
 // app.use('/', express.static(path.join(__dirname, 'public')))
 // app.use('/MessageOnEvent', express.static(path.join(__dirname, 'public/messageOnEvent.html')))
